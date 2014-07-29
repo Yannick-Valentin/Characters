a045051815d88e258d167c718be41541bbd5fbad
Yannick Valentin <yannick@yvalentin.com>
2014-07-29 11:43:43 +0200

    v1

---
+++ app.js
@@ -0,0 +1,48 @@
+// Déclaration des variables
+var button = document.getElementById('convert'),
+	input  = document.getElementById('input'),
+	output = document.getElementById('output');
+
+
+// Fonction de convertion.
+escapeHtmlEntities = function (txt) {
+
+	// E
+	txt = txt.replace(/é/g, '&eacute;');
+	txt = txt.replace(/è/g, '&egrave;');
+	txt = txt.replace(/ê/g, '&ecirc;');
+	txt = txt.replace(/ë/g, '&euml;');
+
+	// A
+	txt = txt.replace(/à/g, '&agrave;');
+
+	// I
+	txt = txt.replace(/ï/g, '&iuml;');
+	txt = txt.replace(/î/g, '&icirc;');
+
+	// O
+	txt = txt.replace(/ö/g, '&ouml;');
+	txt = txt.replace(/ô/g, '&ocirc;');
+
+	// C
+	txt = txt.replace(/ç/g, '&ccedil;');
+
+	return txt;
+}
+
+// Converti les entités HTML
+button.onclick = function() {	
+
+	output.value = escapeHtmlEntities(input.value);
+
+	return false;
+
+}
+
+// Selectionne tout le contenu au clic
+output.onclick = function() {
+
+	output.focus();
+	output.select();
+
+}
